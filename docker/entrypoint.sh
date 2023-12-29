706b70fc1962b16dce92ec47003c7772f6c6f9ee
Jinyoung Jang <jyjang@uengine.org>
2023-12-29 14:11:13 +0000

    c

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 project_dir="/project"
 
 # Run the gpt engineer script
-gpt-engineer $project_dir "$@" -sh
+gpt-engineer $project_dir "$@"
 
 # Patch the permissions of the generated files to be owned by nobody except prompt file
 for item in "$project_dir"/*; do
